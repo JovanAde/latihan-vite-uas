2412cb3959d69a080007c697e21067fd85d681d2
masroyy18 <royyanhali180402@gmail.com>
2024-04-25 17:08:58 +0700

    update

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,13 @@
 import "./home.scss";
-const Home =() =>{
-    return(
-        <div>Home</div>
-    )
-}
-export default Home
+import Sidebar from "../../components/sidebar/Sidebar";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Sidebar />
+      <div className="homeContainer">container</div>
+    </div>
+  );
+};
+
+export default Home;
