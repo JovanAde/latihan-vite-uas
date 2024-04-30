694c48664ac70dcc63b405ea1e229bbc892f1815
masroyy18 <royyanhali180402@gmail.com>
2024-04-30 09:18:41 +0700

    Update

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,8 @@
 import "./home.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
-import Navbar from "../../components/navbar/Navbarbar";
+import Navbar from "../../components/navbar/Navbar";
+import Widget from "../../components/widget/Widget";
+
 
 const Home = () => {
   return (
