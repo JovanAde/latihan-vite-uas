41cd2660b1c30ad5d6b0f887471d13a0a94773ca
masroyy18 <royyanhali180402@gmail.com>
2024-05-02 15:10:10 +0700

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,29 @@
-import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import List from "./pages/list/List";
 import Single from "./pages/single/Single";
 import New from "./pages/new/New";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { productInputs, userInputs } from "./formsource";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/">
-            <Route index element={<Home />}></Route>
-            <Route path="login" element={<Login />}></Route>
-            <Route path="users">
-              <Route index element={<List />}></Route>
-              <Route path=":userId" element={<Single />}></Route>
-              <Route path="new" element={<New />}></Route>
-            </Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/users">
+          <Route index element={<List />} />
+          <Route path="new" element={<New inputs={userInputs} title="Add New User" />} />
+          <Route path=":userId" element={<Single />} />
+        </Route>
+        <Route path="/products">
+          <Route index element={<List />} />
+          <Route path="new" element={<New inputs={productInputs} title="Add New Product" />} />
+          <Route path=":productId" element={<Single />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,7 +1,18 @@
 import "./list.scss";
-const List =() =>{
-    return(
-        <div>List</div>
-    )
-}
-export default List
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import Datatable from "../../components/datatable/Datatable";
+
+const List = () => {
+  return (
+    <div className="list">
+      <Sidebar />
+      <div className="listContainer">
+        <Navbar />
+        <Datatable/>
+      </div>
+    </div>
+  );
+};
+
+export default List;
